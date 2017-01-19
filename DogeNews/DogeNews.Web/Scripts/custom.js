73c763ec67fbc5de120a0d65351a793aa8982f7d
Sparx92 <stefankoserkov@gmail.com>
2017-01-19 18:22:42 +0200

    Adding ninject.web

--- DogeNews/DogeNews.Web/Scripts/custom.js
+++ DogeNews/DogeNews.Web/Scripts/custom.js
@@ -1,4 +1,12 @@
 ﻿$(document).ready(function () {
   $(".button-collapse").sideNav();
   $(".dropdown-button").dropdown({ hover: false });
+
+  var url = window.location;
+  $('nav').find('.active').removeClass('active');
+  $('nav li a').each(function () {
+    if (this.href == url) {
+      $(this).parent().addClass('active');
+    }
+  });
 });
