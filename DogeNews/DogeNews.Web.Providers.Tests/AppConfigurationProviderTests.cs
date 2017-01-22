f9413bd564cfc36d90dc80a618484f295ddd5c76
Sparx92 <stefankoserkov@gmail.com>
2017-01-22 18:26:52 +0200

    Adding App Configuration Provider tests.

---
+++ DogeNews/DogeNews.Web.Providers.Tests/AppConfigurationProviderTests.cs
@@ -0,0 +1,42 @@
+﻿using DogeNews.Web.Providers.Config;
+using NUnit.Framework;
+
+namespace DogeNews.Web.Providers.Tests
+{
+    [TestFixture]
+    public class AppConfigurationProviderTests
+    {
+        [Test]
+        public void AuthCookieName_ShouldNotThrow()
+        {
+            var appConfigProvider = new AppConfigurationProvider();
+
+            Assert.DoesNotThrow(() =>
+            {
+                var authCookieName = appConfigProvider.AuthCookieName;
+            });
+        }
+        
+        [Test]
+        public void EncryptionKey_ShouldNotThrow()
+        {
+            var appConfigProvider = new AppConfigurationProvider();
+
+            Assert.DoesNotThrow(() =>
+            {
+                var encryptionKey = appConfigProvider.EncryptionKey;
+            });
+        }
+
+        [Test]
+        public void AdminPassword_ShouldNotThrow()
+        {
+            var appConfigProvider = new AppConfigurationProvider();
+            
+            Assert.DoesNotThrow(()=>
+            {
+                var adminPassword = appConfigProvider.AdminPassword;
+            });
+        }
+    }
+}
