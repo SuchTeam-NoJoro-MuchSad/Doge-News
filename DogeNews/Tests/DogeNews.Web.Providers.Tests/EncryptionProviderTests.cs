f520f1fb0bd26884749b6660bb04663c0da83b1d
belchevgb <belchev.gb@gmail.com>
2017-01-25 13:41:00 +0200

    The repository is reordered.

---
+++ DogeNews/Tests/DogeNews.Web.Providers.Tests/EncryptionProviderTests.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Text;
+using System.Web.Security;
+
+using DogeNews.Web.Providers.Contracts;
+using DogeNews.Web.Providers.Encryption;
+
+using NUnit.Framework;
+
+namespace DogeNews.Web.Providers.Tests
+{
+    [TestFixture]
+    public class EncryptionProviderTests
+    {
+        private IEncryptionProvider encryptionProvider;
+
+        [SetUp]
+        public void Init()
+        {
+            this.encryptionProvider = new EncryptionProvider();
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        public void Encrypt_ShouldThrowArgumentNullExceptionWhenTextIsNullOrEmpty(string text)
+        {
+            var exception = Assert.Throws<ArgumentNullException>(() => this.encryptionProvider.Encrypt(text, "key"));
+            Assert.AreEqual("text", exception.ParamName);
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        public void Encrypt_ShouldThrowArgumentNullExceptionWhenKeyIsNullOrEmpty(string key)
+        {
+            var exception = Assert.Throws<ArgumentNullException>(() => this.encryptionProvider.Encrypt("text", key));
+            Assert.AreEqual("key", exception.ParamName);
+        }
+
+        [Test]
+        public void Encrypt_ShouldReturnCorrectlyEncryptedString()
+        {
+            string text = "text";
+            string key = "key";
+            byte[] encryptedTextBytes = MachineKey.Protect(Encoding.UTF8.GetBytes(text), key);
+
+            string expected = Convert.ToBase64String(encryptedTextBytes);
+            string actual = this.encryptionProvider.Encrypt(text, key);
+
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        public void Decrypt_ShouldThrowArgumentNullExceptionWhenTextIsNullOrEmpty(string text)
+        {
+            var exception = Assert.Throws<ArgumentNullException>(() => this.encryptionProvider.Encrypt(text, "key"));
+            Assert.AreEqual("text", exception.ParamName);
+        }
+
+        [TestCase(null)]
+        [TestCase("")]
+        public void Decrypt_ShouldThrowArgumentNullExceptionWhenKeyIsNullOrEmpty(string key)
+        {
+            var exception = Assert.Throws<ArgumentNullException>(() => this.encryptionProvider.Encrypt("text", key));
+            Assert.AreEqual("key", exception.ParamName);
+        }
+
+        [Test]
+        public void Decrypt_ShouldReturnCorrectlyDecryptedString()
+        {
+            string text = "text";
+            string key = "key";
+            byte[] encryptedTextBytes = MachineKey.Protect(Encoding.UTF8.GetBytes(text), key);
+            string encryptedText = Convert.ToBase64String(encryptedTextBytes);
+            byte[] decryptedBytes = MachineKey.Unprotect(Convert.FromBase64String(encryptedText), key);
+            
+            string expected = Encoding.UTF8.GetString(decryptedBytes);
+            string actual = this.encryptionProvider.Decrypt(encryptedText, key);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ DogeNews/Web/DogeNews.Web/Scripts/User/settings.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    'use strict';
+
+    document
+        .getElementById('MainContent_Message')
+        .addEventListener('DOMAttrModified', e => {
+            if (e.target.style.display !== 'none') {
+                setTimeout(() => $this.hide(), 3000);
+            }
+        });
+} ());
