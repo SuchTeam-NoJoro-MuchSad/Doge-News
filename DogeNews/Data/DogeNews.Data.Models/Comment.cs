707131243f1f9987938aa9a9d6b3bd3e225c93c9
Sparx92 <stefankoserkov@gmail.com>
2017-01-31 16:56:45 +0200

    Updated comment model so it has CreatedOn and Deleted on properties. Created CommentWebModel.

--- DogeNews/Data/DogeNews.Data.Models/Comment.cs
+++ DogeNews/Data/DogeNews.Data.Models/Comment.cs
@@ -1,9 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace DogeNews.Data.Models
 {
     public class Comment 
     {
+        public Comment()
+        {
+            this.CreatedOn = DateTime.UtcNow;
+        }
+
         public int Id { get; set; }
 
         [Required]
@@ -13,5 +19,9 @@ namespace DogeNews.Data.Models
         [MinLength(2)]
         [MaxLength(250)]
         public string Content { get; set; }
+
+        public DateTime? CreatedOn { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
     }
 }
---
+++ DogeNews/Web/DogeNews.Web.Models/CommentWebModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DogeNews.Web.Models
+{
+    public class CommentWebModel
+    {
+        public int Id { get; set; }
+
+        public UserWebModel User { get; set; }
+
+        public string Content { get; set; }
+
+        public DateTime? CreatedOn { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+    }
+}
