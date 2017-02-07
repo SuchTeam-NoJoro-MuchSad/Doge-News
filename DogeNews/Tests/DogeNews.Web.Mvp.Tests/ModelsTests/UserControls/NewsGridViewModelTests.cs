d6f500a48d17b65f695c422495836bec6fa7d038
belchevgb <belchev.gb@gmail.com>
2017-02-07 15:02:01 +0200

    Added tests for ChangePageEventArgs

---
+++ DogeNews/Tests/DogeNews.Web.Mvp.Tests/ModelsTests/UserControls/NewsGridViewModelTests.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+using DogeNews.Data.Models;
+using DogeNews.Web.DataSources.Contracts;
+using DogeNews.Web.Models;
+using DogeNews.Web.Mvp.UserControls.NewsGrid;
+
+using Moq;
+using NUnit.Framework;
+
+namespace DogeNews.Web.Mvp.Tests.ModelsTests.UserControls
+{
+    [TestFixture]
+    public class NewsGridViewModelTests
+    {
+        [Test]
+        public void NewsCountShouldReturnTheSetValue()
+        {
+            var model = new NewsGridViewModel();
+            int newsCount = 5;
+
+            model.NewsCount = newsCount;
+            Assert.AreEqual(newsCount, model.NewsCount);
+        }
+
+        [Test]
+        public void PageSizeShouldReturnTheSetValue()
+        {
+            var model = new NewsGridViewModel();
+            int pageSize = 5;
+
+            model.PageSize = pageSize;
+            Assert.AreEqual(pageSize, model.PageSize);
+        }
+
+        [Test]
+        public void CurrentPageNewsShouldReturnTheSetValue()
+        {
+            var model = new NewsGridViewModel();
+            IEnumerable<NewsWebModel> currentPageNews = new List<NewsWebModel>();
+
+            model.CurrentPageNews = currentPageNews;
+            Assert.AreEqual(currentPageNews, model.CurrentPageNews);
+        }
+
+        [Test]
+        public void NewsDataSourceShouldReturnTheSetValue()
+        {
+            var model = new NewsGridViewModel();
+            var newsDataSource = new Mock<IDataSource<NewsItem, NewsWebModel>>();
+
+            model.NewsDataSource = newsDataSource.Object;
+            Assert.AreEqual(newsDataSource.Object, model.NewsDataSource);
+        }
+    }
+}
