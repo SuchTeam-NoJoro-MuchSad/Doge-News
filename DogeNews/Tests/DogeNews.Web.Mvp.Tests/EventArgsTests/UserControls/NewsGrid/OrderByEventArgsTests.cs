92e9774e57f6b90ea4e77979d16e9bc2db1fe8f6
belchevgb <belchev.gb@gmail.com>
2017-02-07 15:05:40 +0200

    Added tests for OrderByEventArgs

---
+++ DogeNews/Tests/DogeNews.Web.Mvp.Tests/EventArgsTests/UserControls/NewsGrid/OrderByEventArgsTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+using DogeNews.Common.Enums;
+using DogeNews.Web.Mvp.UserControls.NewsGrid.EventArguments;
+using System.Web.UI;
+
+namespace DogeNews.Web.Mvp.Tests.EventArgsTests.UserControls.NewsGrid
+{
+    [TestFixture]
+    public class OrderByEventArgsTests
+    {
+        [Test]
+        public void OrderByShouldReturnTheSetValue()
+        {
+            var eventArgs = new OrderByEventArgs();
+            var orderBy = OrderByType.Ascending;
+
+            eventArgs.OrderBy = orderBy;
+            Assert.AreEqual(orderBy, eventArgs.OrderBy);
+        }
+
+        [Test]
+        public void ViewStateShouldReturnTheSetValue()
+        {
+            var eventArgs = new OrderByEventArgs();
+            var viewState = new StateBag();
+
+            eventArgs.ViewState = viewState;
+            Assert.AreEqual(viewState, eventArgs.ViewState);
+        }
+    }
+}
