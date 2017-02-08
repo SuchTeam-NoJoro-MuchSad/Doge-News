fec40534d80cd3fb9f2fd108759cd78857e02cb1
Sparx92 <stefankoserkov@gmail.com>
2017-02-08 15:16:45 +0200

    Adding tests for HttpUtilityService

---
+++ DogeNews/Tests/DogeNews.Web.Services.Tests/HttpTests/HttpUtilityServiceTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using DogeNews.Web.Services.Http;
+using NUnit.Framework;
+
+namespace DogeNews.Web.Services.Tests.HttpTests
+{
+    [TestFixture]
+    public class HttpUtilityServiceTests
+    {
+        [Test]
+        public void ParseQueryString_ShouldReturnCorrectHttpValueCollection()
+        {
+            var expectedCount = 2;
+
+            var service = new HttpUtilityService();
+            var result = service.ParseQueryString("name=Sports&page=2");
+
+            Assert.AreEqual(expectedCount, result.Count);
+        }
+
+        [Test]
+        public void ParseQueryString_ShouldThrowArgumentNullException_WhenThePassedParameterIsNull()
+        {
+            var service = new HttpUtilityService();
+
+            Assert.Throws<ArgumentNullException>(() => service.ParseQueryString(null));
+        }
+    }
+}
