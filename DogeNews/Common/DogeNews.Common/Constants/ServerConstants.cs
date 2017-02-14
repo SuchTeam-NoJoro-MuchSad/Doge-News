2f1a92f1f2924bd178080c96f6f909441bd72506
belchevgb <belchev.gb@gmail.com>
2017-02-14 10:39:33 +0200

    Added server constants

---
+++ DogeNews/Common/DogeNews.Common/Constants/ServerConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace DogeNews.Common.Constants
+{
+    public class ServerConstants
+    {
+        public const string DataAssembly = "DogeNews.Data";
+        public const string ProvidersAssembly = "DogeNews.Web.Providers";
+        public const string ServicesAssembly = "DogeNews.Web.Services";
+        public const string DataSourcesAssembly = "DogeNews.Web.DataSources";
+    }
+}
