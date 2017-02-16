e2853a6862bb2daaba2e4f7dca3ddda6f04c6cb8
belchevgb <belchev.gb@gmail.com>
2017-02-16 08:34:21 +0200

    Restored http services tests

---
+++ DogeNews/Tests/DogeNews.Services.Http.Tests/HttpResponseServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using DogeNews.Services.Http;
+
+using NUnit.Framework;
+
+namespace DogeNews.Web.Services.Tests.HttpTests
+{
+    [TestFixture]
+    public class HttpResponseServiceTests
+    {
+        [Test, Ignore("HttpContext is not instanced by the test (only by ASP), and this test will aways throw NullArgumentException")]
+        public void Clear_ShouldNotThrow()
+        {
+            var service = new HttpResponseService();
+            Assert.DoesNotThrow(()=> service.Clear());
+        }
+    }
+}
---
+++ DogeNews/Tests/DogeNews.Services.Http.Tests/HttpServerUtilityServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Web;
+
+using DogeNews.Services.Http;
+
+using NUnit.Framework;
+
+namespace DogeNews.Web.Services.Tests.HttpTests
+{
+    [TestFixture]
+    public class HttpServerUtilityServiceTests
+    {
+        [Test,Ignore("HttpContext is not instanced by the test (only by ASP), and this test will aways throw NullArgumentException")]
+        public void HttpUtilityService_ShouldThrowHttpException_WhenInputArgumentIsNull()
+        {
+            var service = new HttpServerUtilityService();
+            Assert.Throws<HttpException>(() => service.MapPath(null));
+        }
+    }
+}
