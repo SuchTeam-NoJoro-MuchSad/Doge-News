a892b327913b6f0a79ebfcdda0e54ed2b667f4d2
belchevgb <belchev.gb@gmail.com>
2017-02-16 08:38:19 +0200

    Restored common services tests

---
+++ DogeNews/Tests/DogeNews.Services.Common.Tests/DateTimeProviderTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using NUnit.Framework;
+
+using DogeNews.Services.Common;
+
+namespace DogeNews.Web.Providers.Tests
+{
+    [TestFixture]
+    public class DateTimeProviderTests
+    {
+        [Test]
+        public void Now_ShouldReturnDateTimeType()
+        {
+            var dateTimeProvider = new DateTimeProvider();
+            var result = dateTimeProvider.Now;
+
+            Assert.AreEqual(typeof(DateTime), result.GetType());
+        }
+
+        [Test]
+        public void Now_ShouldReturnDateTimeUTCNow()
+        {
+            var dateTimeProvider = new DateTimeProvider();
+            var result = dateTimeProvider.Now;
+
+            Assert.AreEqual(typeof(DateTime), result.GetType());
+            Assert.AreEqual(result.Year, DateTime.UtcNow.Year);
+            Assert.AreEqual(result.Month, DateTime.UtcNow.Month);
+            Assert.AreEqual(result.Day, DateTime.UtcNow.Day);
+            Assert.AreEqual(result.Hour, DateTime.UtcNow.Hour);
+            Assert.AreEqual(result.Minute, DateTime.UtcNow.Minute);
+            Assert.AreEqual(result.Second, DateTime.UtcNow.Second);
+        }
+    }
+}
---
+++ DogeNews/Tests/DogeNews.Services.Common.Tests/MapperProviderTests.cs
@@ -0,0 +1,25 @@
+﻿using AutoMapper;
+
+using DogeNews.Services.Common;
+
+using NUnit.Framework;
+
+namespace DogeNews.Web.Providers.Tests
+{
+    [TestFixture]
+    public class MapperProviderTests
+    {
+        [Test]
+        public void Instance_ShouldBeOfTypeMapper()
+        {
+            var mapperProvider = new MapperProvider();
+
+            mapperProvider.Instance =
+                new Mapper(new MapperConfiguration(delegate (IMapperConfigurationExpression expression) { }));
+
+            var mapperProviderInstance = mapperProvider.Instance;
+
+            Assert.AreEqual(mapperProviderInstance.GetType(), typeof(Mapper));
+        }
+    }
+}
